7a5a32f73946653df3dfe0ce3cdfd1e4ad1c35dc
Codec <c13o42d98e00c87@gmail.com>
2025-05-06 17:28:12 +0900

    Additional Initial settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import { defineConfig } from "eslint/config"
 
 
 export default defineConfig([
@@ -10,4 +10,13 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-]);
+  // rules ここから -----------------------
+  {
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "semi": [2, "never"],
+      "comma-dangle": [2, "never"]
+    }
+  }
+  // rules ここまで -----------------------
+])
