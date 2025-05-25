50327dcd9b591fbee384cda6049a604fe202830d
Codec <c13o42d98e00c87@gmail.com>
2025-05-25 14:45:01 +0900

    Implement sound playing and selection function

--- src/app/bell/bellSelection.tsx
+++ src/app/bell/bellSelection.tsx
@@ -1,50 +1,125 @@
-import { JSX } from "react"
+import { JSX, useState } from "react"
 import { View, StyleSheet, TouchableOpacity, Image, Dimensions } from "react-native"
 import Entypo from '@expo/vector-icons/Entypo'
+import { useAudioPlayer } from "expo-audio"
+import { Feather } from "@expo/vector-icons"
 
 const BellSelect = (): JSX.Element => {
   const windowWidth = Dimensions.get('window').width
   const windowHeight = Dimensions.get('window').height
   const windowMinLen = Math.min(windowWidth, windowHeight)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [selectNo, setSelectNo] = useState(2)
+
+  const audioSource = require('../../../assets/bellSound/sample.mp3')
+  const player = useAudioPlayer(audioSource)
+
+  const playSound = async () => {
+    setIsPlaying(true)
+    player.seekTo(0)
+    player.play()
+    await wait(player.duration)
+    setIsPlaying(false)
+  }
+
+  const wait = async (second: number) => {
+    return new Promise(resolve => setTimeout(resolve, 1000 * second))
+  }
+
+  const selection = (No: number): void => {
+    setSelectNo(No)
+  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.bellContainer}>
-        <TouchableOpacity style={styles.bellTouchArea}>
-          <Image
-            style={styles.bellImage}
-            source={require('../../../assets/bellImage/bell1.png')}
-          />
-          <TouchableOpacity style={styles.playButtonTouchArea}>
-            <Entypo name="controller-play" size={windowMinLen/2/2} color="rgba(0,0,0,0.4)" />
+      <View style={styles.flexWrap}>
+
+        <View
+          style={ selectNo === 1 ? [styles.bellContainer, {borderWidth: 3, borderColor: "#00C0C0"}] : styles.bellContainer }>
+          <TouchableOpacity
+            style={ isPlaying ? [styles.bellTouchArea, {backgroundColor: "white"}] : styles.bellTouchArea}
+            onPress={() => selection(1)}>
+            <Image
+              style={ isPlaying ? [styles.bellImage, {filter: 'grayscale(0%)'}] : styles.bellImage }
+              source={require('../../../assets/bellImage/bell1.png')}
+            />
+            <TouchableOpacity style={styles.playButtonTouchArea} onPress={playSound}>
+              <Entypo name="controller-play"
+                size={windowMinLen/2/2}
+                color={ isPlaying ? 'rgba(0,0,0,0.0)' : 'rgba(0,0,0,0.4)' }
+              />
+            </TouchableOpacity>
+            <View style={styles.checkboxArea}>
+              <Feather
+                name={selectNo === 1 ? "check-circle" : "circle"}
+                size={24}
+                color={selectNo === 1 ? "#00C0C0" : "rgba(0,0,0,0.4)"} />
+            </View>
           </TouchableOpacity>
-          <View style={styles.checkboxArea}>
-            <Entypo name="circle" size={24} color="rgba(0,0,0,0.4)" />
-          </View>
-        </TouchableOpacity>
+        </View>
+
+        <View style={styles.bellContainer}>
+          <TouchableOpacity style={styles.bellTouchArea}>
+            <Image
+              style={styles.bellImage}
+              source={require('../../../assets/bellImage/bell1.png')}
+            />
+            <TouchableOpacity style={styles.playButtonTouchArea}>
+              <Entypo name="controller-play" size={windowMinLen/2/2} color="rgba(0,0,0,0.4)" />
+            </TouchableOpacity>
+            <View style={styles.checkboxArea}>
+              <Entypo name="circle" size={24} color="rgba(0,0,0,0.4)" />
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.bellContainer}>
+          <TouchableOpacity style={styles.bellTouchArea}>
+            <Image
+              style={styles.bellImage}
+              source={require('../../../assets/bellImage/bell1.png')}
+            />
+            <TouchableOpacity style={styles.playButtonTouchArea}>
+              <Entypo name="controller-play" size={windowMinLen/2/2} color="rgba(0,0,0,0.4)" />
+            </TouchableOpacity>
+            <View style={styles.checkboxArea}>
+              <Entypo name="circle" size={24} color="rgba(0,0,0,0.4)" />
+            </View>
+          </TouchableOpacity>
+        </View>
+
+
       </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+let styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#eeeeee',
   },
+  flexWrap : {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '100%'
+  },
   bellContainer: {
-    width: '50%',
-    aspectRatio: 1/1,
+    width: Math.min(Dimensions.get('window').width,Dimensions.get('window').height)/2,
+    height: Math.min(Dimensions.get('window').width,Dimensions.get('window').height)/2,
     position: 'relative',
     borderWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.3)'
+    borderColor: 'rgba(255, 255, 255, 0.4)'
   },
   bellTouchArea: {
     width: '100%',
     height: '100%',
     position: 'absolute',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)'
   },
   playButtonTouchArea: {
     width: '50%',
@@ -55,7 +130,8 @@ const styles = StyleSheet.create({
   },
   bellImage: {
       width: '90%',
-      objectFit: 'contain'
+      objectFit: 'contain',
+      filter: 'grayscale(70%)'
   },
   checkboxArea: {
     position: 'absolute',
